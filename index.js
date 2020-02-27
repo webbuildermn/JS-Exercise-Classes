19c168f4d96d022991c703019caa8655471597ab
Webbuildermn <gerald.ryan40@yahoo.com>
2020-02-27 16:04:01 -0600

    Got many finished

--- index.js
+++ index.js
@@ -133,7 +133,14 @@ myPorchse.drive(100)
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
+  constructor(args){
+    this.name = args.name
+    this.age = args.age
+    this.location = args.location
+  }
+  speak(){
+    return `Hello my name is ${this.name}. I am from ${this.location}`
+  }
 }
 
 /*
@@ -150,8 +157,19 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
-
+class Instructor extends Lambdasian {
+  constructor(args){
+    super(args)
+    this.specialty = args.specialty
+    this.favLanguage = args.favLanguage
+    this.catchPhrase = args.catchPhrase
+  }
+  demo(subj){
+    return `Today we are learning about ${subj}`
+  }
+  grade(student, subject){
+    return `${student.name} receives a perfect score on ${subject}`
+  }
 }
 
 /*
@@ -169,7 +187,22 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
+class Student extends Lambdasian{
+  constructor(args){
+    super(args)
+    this.previousBackground = args.previousBackground
+    this.className = args.className
+    this.favSubjects = args.favSubjects
+  }
+  listSubjects(){
+    return `Loving ${this.favSubjects}`
+  }
+  PRAssignment(subject){
+    return `${this.name} has submitted a PR for ${subject}`
+  }
+  sprintChallenge(subject){
+    return `${this.name} has begun sprint challenge on ${subject}`
+  }
 
 }
 
